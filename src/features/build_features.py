af8f6a2e1ff6d0c3eb71b7cc0a1574429bc403d6
Nyx <riadr5206@gmail.com>
2024-01-29 13:17:22 +0100

    60%

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
-
+from FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 # --------------------------------------------------------------
 # Load data
@@ -23,14 +24,15 @@ plt.rcParams["lines.linewidth"] = 2
 # --------------------------------------------------------------
 
 for col in predictor_columns:
-    df [col] = df[col].interpolate()
-    
+    df[col] = df[col].interpolate()
+
 # --------------------------------------------------------------
 # Calculating set duration
 # --------------------------------------------------------------
 
 df[df["set"] == 25]["acc_y"].plot()
 df[df["set"] == 50]["acc_y"].plot()
+
 
 duration = df[df["set"] == 25].index[-1] - df[df["set"] == 25].index[0]
 duration.seconds
@@ -53,6 +55,7 @@ duration_df.iloc[1] /10
 
 df_lowpass = df.copy()
 LowPass = LowPassFilter()
+
 fs = 1000 / 200
 cutoff = 1.3
 
@@ -72,6 +75,7 @@ for col in  predictor_columns:
     df_lowpass = LowPass.low_pass_filter(df_lowpass, col , fs, cutoff, order=5)
     df_lowpass[col] = df_lowpass[col + "_lowpass"]
     del df_lowpass[col + "_lowpass"]
+    
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
@@ -92,6 +96,7 @@ df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
 subset = df_pca[df_pca["set"] == 35]
 
 subset[["pca_1", "pca_2", "pca_3"]].plot()
+df_pca
 
 # --------------------------------------------------------------
 # Sum of squares attributes
@@ -106,31 +111,141 @@ gyr_r = df_squared["gyr_x"] ** 2 + df_squared["gyr_y"] ** 2 + df_squared["gyr_z"
 df_squared["acc_r"] = np.sqrt(acc_r)
 df_squared["gyr_r"] = np.sqrt(gyr_r)
 
-subset = df_squared[df_squared["set"] == 14]
+subset = df_squared[df_squared["set"] == 28]
+subset
 
 subset[["acc_r", "gyr_r"]].plot(subplots=True)
-df_squared
 
+df_squared
 # --------------------------------------------------------------
 # Temporal abstraction
 # --------------------------------------------------------------
 
+df_temporal = df_squared.copy()
+NumAbs = NumericalAbstraction()
+df_temporal
+
+predictor_columns = predictor_columns + ["acc_r" , "gyr_r"]
+predictor_columns
+
+
+ws = int(1000/200)
+
+for col in predictor_columns:
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
+
+
+    
+df_temporal_list = []
+for x in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == x].copy()
+    for col in predictor_columns:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+    
+df_temporal = pd.concat(df_temporal_list)
+
+subset[["acc_y", "acc_y_temp_mean_ws_5", "acc_y_temp_std_ws_5"]].plot()
+subset[["gyr_y", "gyr_y_temp_mean_ws_5", "gyr_y_temp_std_ws_5"]].plot()
+df_temporal
 
 # --------------------------------------------------------------
 # Frequency features
 # --------------------------------------------------------------
 
+df_freq = df_temporal.copy().reset_index()
+df_freq
+FreqAbs = FourierTransformation()
 
+fs = int(1000/200)
+ws  = int(2800/200)
+
+df_freq = FreqAbs.abstract_frequency(df_freq, ["acc_y"], ws, fs)
+df_freq.columns
+
+subset = df_freq[df_freq["set"] == 15]
+subset
+subset[["acc_y"]].plot()
+subset[
+        ["acc_y_max_freq",
+        "acc_y_freq_weighted",
+        "acc_y_pse",
+        "acc_y_freq_1.429_Hz_ws_14",
+        "acc_y_freq_2.5_Hz_ws_14"]
+       ].plot()
+
+df_freq_list = []
+for s in df_freq["set"].unique():
+    #print[f"Appling Fourrier Transformation to set {s}"]
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, predictor_columns, ws, fs)
+    df_freq_list.append(subset)
+
+df_freq = pd.concat(df_freq_list).set_index("epoch (ms)", drop=True)
+df_freq
 # --------------------------------------------------------------
 # Dealing with overlapping windows
 # --------------------------------------------------------------
 
+df_freq = df_freq.dropna()
+df_freq = df_freq.iloc[::2]
 
 # --------------------------------------------------------------
 # Clustering
 # --------------------------------------------------------------
 
+df_cluster = df_freq.copy()
+df_cluster
+
+cluster_columns = ["acc_x","acc_y","acc_z"]
+k_values = range(2, 10)
+inertias = []
+
+for k in k_values:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters=k, n_init=20, random_state=0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)
+plt.xlabel("k")
+plt.ylabel("Sum of squared distances")
+plt.show()
+
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+df_cluster
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
+
+
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection="3d")
+for l in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == l]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=l)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_zlabel("Z-axis")
+plt.legend()
+plt.show()
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+df_cluster.to_pickle("../../data/interim/03_data_features.pkl")
+df_cluster
